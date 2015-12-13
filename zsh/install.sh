a80b734a47c674010f1d0f3340209f93fea02e21
Jonathan Borg <jonathan@x3m.io>
2015-12-13 17:05:30 +0700

    removed inline zpresto

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-echo "zshell setting as default shell for you."
+echo "Installing zpresto and setting zshell as default shell"
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 chsh -s /bin/zsh
+
+rm "${ZDOTDIR:-$HOME}/.zpreztorc"
+rm "${ZDOTDIR:-$HOME}/.zshrc"
